c0dcb35dafd31ed759e28f28d3d1e73addaf50be
Andreazo Silva <andreazocoisas@gmail.com>
2020-10-01 21:10:57 -0300

    Subindo projeto para o github

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,29 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../erros/missing-param-error'
+import { InvalidParamError } from '../erros/invalid-param-error'
+import { badRequest } from '../helpers/http-helpers'
+import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+
+export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+    }
+
+    const { email } = httpRequest.body
+    const isValid = this.emailValidator.isValid(email)
+    if (!isValid) return badRequest(new InvalidParamError('email'))
+
+    return {
+      statusCode: 200,
+      body: {}
+    }
+  }
+}
