36e351c2d4067ecfb0b969f6685885114af952d1
Andreazo Silva <andreazocoisas@gmail.com>
2020-10-03 15:11:31 -0300

    Criado email-validator-adapteter e seus testes

---
+++ src/utils/email-validator-adapter.ts
@@ -0,0 +1,7 @@
+import validator from 'validator'
+import { EmailValidator } from '../presentation/protocols/email-validator'
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
